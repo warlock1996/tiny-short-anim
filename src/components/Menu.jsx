a943c69d8fcbd5aa985becb5a1cba033e6952a53
warlock1996 <arslanali.921996@gmail.com>
2022-09-17 14:36:02 +0500

    fix: top navigation

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 
-const Menu = () => {
+const Menu = (props) => {
 	return (
 		<div>
 			<div className='relative inline-block w-[35px] h-[20px] px-[5px] py-0'>
-				<div className='absolute block bg-white w-[25px] h-[3px] rounded-[100px] after:content-[" "] after:block after:top-[16px] after:bg-white after:absolute after:w-[25px] after:h-[3px] after:rounded-[100px] after:bg-white before:w-[20px] before:absolute before:h-[3px] before:rounded-[100] before:bg-white before:content-[" "] before:block before:top-[8px]'></div>
+				<div
+					className={`absolute block ${props.fill} w-[25px] h-[3px] rounded-[100px] after:content-[" "] after:block after:top-[16px] after:${props.fill} after:absolute after:w-[25px] after:h-[3px] after:rounded-[100px] after:${props.fill} before:w-[20px] before:absolute before:h-[3px] before:rounded-[100] before:${props.fill} before:content-[" "] before:block before:top-[8px]`}
+				></div>
 			</div>
 		</div>
 	)
--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -4,39 +4,58 @@ import MessageIcon from '../assets/icons/message.svg'
 import { ReactComponent as MessageIconComponent } from '../assets/icons/message.svg'
 import Menu from '../components/Menu'
 import Button from '../components/Button.jsx'
+import { useEffect } from 'react'
+import { useState } from 'react'
+import './Navigation.css'
 
-const Navigtion = () => {
+const Navigation = () => {
+	const [hasScrolled, setHasScrolled] = useState(false)
+
+	useEffect(() => {
+		let timeout = null
+		window.addEventListener('scroll', (event) => {
+			timeout = setTimeout(() => {
+				clearTimeout(timeout)
+				if (window.scrollY > 200) setHasScrolled(true)
+				if (window.scrollY < 200) setHasScrolled(false)
+			}, 400)
+		})
+	}, [])
 	return (
-		<div className='bg-primaryDarker sticky top-0'>
+		<div
+			className={`transition-colors duration-[500ms] bg-primaryDarker text-white z-[10] w-full ${
+				hasScrolled ? '!bg-white shadow fixed text-primaryDarker' : ''
+			}`}
+		>
 			<div className='mx-auto px-10 xl:container xl:px-40'>
 				<nav className='hidden md:flex items-center justify-between py-10'>
 					<div>
-						<Logo fill='white' />
+						<Logo fill={hasScrolled ? '#0f4848' : 'white'} />
 					</div>
 
 					<div>
-						<ul className='list-none flex gap-5 font-normal text-[16px] p-0 m-0'>
-							<li className='uppercase text-white '>services</li>
-							<li className='uppercase text-white '>about us</li>
-							<li className='uppercase text-white '>projects</li>
-							<li className='uppercase text-white '>blog</li>
+						<ul className='list-none flex gap-5 font-normal text-[16px] p-0 m-0 uppercase cursor-pointer'>
+							<li className={`text-${hasScrolled ? 'bg-primaryDarker' : 'white'}`}>services</li>
+							<li className={`text-${hasScrolled ? 'bg-primaryDarker' : 'white'}`}>about us</li>
+							<li className={`text-${hasScrolled ? 'bg-primaryDarker' : 'white'}`}>projects</li>
+							<li className={`text-${hasScrolled ? 'bg-primaryDarker' : 'white'}`}>blog</li>
 						</ul>
 					</div>
 
 					<div>
-						<Button icon={MessageIcon} outline>
+						<Button icon={MessageIcon} outline={!hasScrolled}>
 							get in touch
 						</Button>
 					</div>
 				</nav>
 				<nav className='md:hidden flex items-center justify-between py-10'>
-					<Menu />
-					<Logo fill='white' className='w-[100px] h-[12px]' />
-					<MessageIconComponent className='w-[23px] h-[19px]' />
+					<Menu fill={hasScrolled ? 'bg-[#0f4848]' : 'bg-white'} />
+					<Logo fill={hasScrolled ? '#0f4848' : 'white'} className='w-[100px] h-[12px]' />
+					<MessageIconComponent fill={hasScrolled ? '#0f4848' : '#fff'} className='w-[23px] h-[19px]' />
 				</nav>
 			</div>
 		</div>
 	)
 }
 
-export default Navigtion
+export default Navigation
